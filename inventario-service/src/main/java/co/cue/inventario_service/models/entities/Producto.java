2d49adc5433d4eb08f9a8f9f2b2aa821a1134439
Nicolás <ncuervo_175@cue.edu.co>
2025-11-02 07:30:15 -0500

    Modificando categoria productos

--- inventario-service/src/main/java/co/cue/inventario_service/models/entities/Producto.java
+++ inventario-service/src/main/java/co/cue/inventario_service/models/entities/Producto.java
@@ -8,6 +8,10 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+
 @Entity
 @Table(name = "productos")
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -26,20 +30,24 @@ public abstract class Producto {
     @Column(nullable = false)
     private Double precio;
 
-
     @Column(nullable = false)
     private Integer stockActual;
 
     private Integer stockMinimo;
     private String ubicacion;
 
+
+    @Column(nullable = false)
+    private boolean activo = true;
+
+
     private boolean disponibleParaVenta;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "categoria_id", nullable = false)
+
+    @JsonIgnoreProperties("productos")
     private Categoria categoria;
-
-
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
