c6c9e9707ade22f8b70142711afd12cbbc919562
Scabezaspaz <scabezas_133@cue.edu.co>
2025-11-06 21:58:11 -0500

    Correcion detalles Mascotas

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -7,15 +7,15 @@ CREATE DATABASE IF NOT EXISTS inventario_service;
 CREATE DATABASE IF NOT EXISTS facturas_service;
 
 -- Crear el usuario con mysql_native_password (compatible con el conector)
-CREATE USER IF NOT EXISTS 'CuervoAdmin'@'%' IDENTIFIED WITH mysql_native_password BY '371211';
+CREATE USER IF NOT EXISTS 'Scabezas'@'%' IDENTIFIED WITH mysql_native_password BY '371211';
 
 -- Otorgar permisos
-GRANT ALL PRIVILEGES ON auth_service.* TO 'CuervoAdmin'@'%';
-GRANT ALL PRIVILEGES ON administration_service.* TO 'CuervoAdmin'@'%';
-GRANT ALL PRIVILEGES ON mascotas_service.* TO 'CuervoAdmin'@'%';
-GRANT ALL PRIVILEGES ON citas_service.* TO 'CuervoAdmin'@'%';
-GRANT ALL PRIVILEGES ON historias_service.* TO 'CuervoAdmin'@'%';
-GRANT ALL PRIVILEGES ON inventario_service.* TO 'CuervoAdmin'@'%';
-GRANT ALL PRIVILEGES ON facturas_service.* TO 'CuervoAdmin'@'%';
+GRANT ALL PRIVILEGES ON auth_service.* TO 'Scabezas'@'%';
+GRANT ALL PRIVILEGES ON administration_service.* TO 'Scabezas'@'%';
+GRANT ALL PRIVILEGES ON mascotas_service.* TO 'Scabezas'@'%';
+GRANT ALL PRIVILEGES ON citas_service.* TO 'Scabezas'@'%';
+GRANT ALL PRIVILEGES ON historias_service.* TO 'Scabezas'@'%';
+GRANT ALL PRIVILEGES ON inventario_service.* TO 'Scabezas'@'%';
+GRANT ALL PRIVILEGES ON facturas_service.* TO 'Scabezas'@'%';
 
 FLUSH PRIVILEGES;
---
+++ mascotas-service/src/main/java/co/cue/mascotas_service/repository/MascotaRepository.java
@@ -0,0 +1,25 @@
+package co.cue.mascotas_service.repository;
+
+import co.cue.mascotas_service.model.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Long> {
+
+    List<Mascota> findByDuenoId(Long duenoId);
+
+
+    List<Mascota> findByEspecie(String species);
+
+    List<Mascota> findByActiveTrue();
+
+    @Query("SELECT p FROM Mascota p WHERE LOWER(p.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))")
+    List<Mascota> searchByName(@Param("nombre") String name);
+
+
+}
