24b1bc5c6c5e242d5c33688bc108bfd30f855690
Nicolás <ncuervo_175@cue.edu.co>
2025-11-08 12:08:31 -0500

    se agrega archivo security para recibir tokens

---
+++ mascotas-service/src/main/java/co/cue/mascotas_service/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package co.cue.mascotas_service.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    private static final String MASCOTAS_API_PATH = "/api/mascotas/**";
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+        SecretKey key = new SecretKeySpec(keyBytes, "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(key).build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter()) // Aplicamos nuestro conversor
+                        )
+                );
+
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.GET, MASCOTAS_API_PATH).authenticated()
+                        .requestMatchers(HttpMethod.POST, MASCOTAS_API_PATH).permitAll()
+                        .requestMatchers(HttpMethod.PUT, MASCOTAS_API_PATH).permitAll()
+                        .requestMatchers(HttpMethod.DELETE, MASCOTAS_API_PATH).permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtConverter;
+    }
+
+}
