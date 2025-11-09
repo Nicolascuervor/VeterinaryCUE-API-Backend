35e291b39924acca38679e64384c0cd1d5c00ac2
Nicolás <ncuervo_175@cue.edu.co>
2025-11-09 11:49:06 -0500

    Se implementa estructura de microservcio agendamiento-service

---
+++ agendamiento-service/src/main/java/co/cue/agendamiento_service/AgendamientoServiceApplication.java
@@ -0,0 +1,13 @@
+package co.cue.agendamiento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamientoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamientoServiceApplication.class, args);
+	}
+
+}
