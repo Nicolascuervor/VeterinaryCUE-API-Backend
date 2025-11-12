9f87d8d8c05adbe7acacf61a6913561fcf08df41
Nicolás <ncuervo_175@cue.edu.co>
2025-11-11 23:21:58 -0500

    Se configuran CORS para entorno de desarrollo

---
+++ api-gateway/src/main/java/co/cue/api_gateway/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package co.cue.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.List;
+
+/**
+ * (Profesor): Esta es la configuración global de CORS para todo el proyecto.
+ * Spring Cloud Gateway (que es Reactivo) interceptará todas las peticiones
+ * y aplicará estas reglas.
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(List.of("http://localhost:5500"));
+
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
